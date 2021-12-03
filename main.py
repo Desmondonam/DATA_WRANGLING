45e0044ef135b3b2451aa89151c20cdcf2c53241
Desmond Onam <desmondonam@gmail.com>
2021-12-03 11:47:05 +0300

    add main file

---
+++ main.py
@@ -0,0 +1,55 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+
+
+url = "https://www.flipkart.com/search?q=python%20books&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off";
+
+result = requests.get(url)
+doc = BeautifulSoup(result.text, "html.parser")
+#print(doc.prettify)
+
+
+# function to search fro special characters
+def special_characters(string):
+    new_string = re.sub(r"[^a-zA-Z0-9]", "", string)
+    return new_string
+
+# function to search for the data from the tags
+def searching_string(string):
+    pattern = ">(.*?)<img"
+    substring = re.search(pattern, string).group(1)
+    return substring
+
+
+
+# finding the price of items
+prices = doc.find_all(text="₹")
+parent = prices[0].parent
+strong = parent.find('div class="_3I9_wc"')
+print(strong.string)
+
+
+# prices = doc.find_all(text="₹")
+# parent = prices[0].parent
+# strong = parent.find("strong")
+# print(strong)
+
+'''
+# extracting specific items.
+if (type =="price"): # price
+    priceTag = mydiv.find("div", {"class": "_30jeq3"})
+    if (priceTag == None):
+        price = 0
+    else:
+        price = int(float(special_characters(priceTag.text[1:])))
+        print(price)
+
+'''
+
+
+
+
+# with open(r'https://www.flipkart.com/search?q=python%20books&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off', 'r') as html_file:
+#     content = html_file.read()
+#     print(content)
